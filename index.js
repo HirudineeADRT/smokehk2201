a4bb8555cdc6d57914627ca04a064e38832e2275
HirudineeADRT <hirudinee@adroitlogic.com>
2020-01-22 14:50:33 +0530

    added py

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is used to register all your cloud functions in GCP.
+// DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+exports.smokehk2201function = require("./smokehk2201/function.js").handler;
